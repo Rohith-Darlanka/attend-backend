7ca1aed680e574a85d63164485fa86011765d646
Rohith-Darlanka <rohithdarlanka@gmail.com>
2025-11-06 21:25:51 +0530

    Ready for Render deployment

--- attend_backend/settings.py
+++ attend_backend/settings.py
@@ -99,7 +99,7 @@ REST_FRAMEWORK = {
         'core.authentication.CookieJWTAuthentication',
     ),
     'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ),
 }
 
